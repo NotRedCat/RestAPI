81e59e538a422143bea4c02db2f830f4bc93e6e6
noredcat <anastasiaredcat@gmail.com>
2022-10-16 21:31:09 +0700

    added SelenoidTests and ReqresInTests

---
+++ src/test/java/tests/ReqresInTests.java
@@ -0,0 +1,122 @@
+package tests;
+
+import netscape.javascript.JSObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresInTests {
+   /*
+        1. make POST- request to https://reqres.in/api/login
+            with body { "email": "eve.holt@reqres.in", "password": "cityslicka" }
+        2. get response { "token": "QpwL5tke4Pnpja7X4" }
+        3. check token is "QpwL5tke4Pnpja7X4"
+     */
+
+    @Test
+    @DisplayName("Получение информации о пользователе")
+    void SingleUserTest() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://reqres.in/api/users/7")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.first_name", is("Michael"))
+                .body("data.last_name", is("Lawson"));
+    }
+
+
+    @Test
+    @DisplayName("Пользователь не найден")
+    void UserNotFoundTest() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://reqres.in/api/users/45")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("Создание нового пользователя")
+    void CreateUserTest() {
+        String body = "{ \"name\": \"Jack\", \"job\": \"teacher\" }";
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("Jack"))
+                .body("job", is("teacher"));
+    }
+
+ /*   @Test
+    @DisplayName("Создание пустого пользователя" +
+            "Тут я ожидала, что возникнет какая-нибудь ошибка, но пустой пользователь сохранился." +
+            "Думаю это полезный тест, чтобы понять, что в базу сохраняются пустые пользователи, " +
+            "и нужно это исправлять")
+    void CreateEmptyUserTest() {
+        String body = "{}";
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400);
+    }*/
+
+    @Test
+    @DisplayName("Изменение существующего пользователя")
+    void ChangeUserTest() {
+        String body = "{ \"name\": \"Jack\", \"job\": \"teacher\" }";
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .put("https://reqres.in/api/user/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("Jack"))
+                .body("job", is("teacher"));
+    }
+
+    @Test
+    @DisplayName("Удаление пользователя")
+    void DeleteUserTest() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .delete("https://reqres.in/api/users/8")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+
+}
---
+++ src/test/java/tests/SelenoidTests.java
@@ -0,0 +1,113 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+
+public class SelenoidTests {
+    /*
+     1. make request to https://selenoid.autotests.cloud/status
+     2. get response {"total":20,"used":0,"queued":0,"pending":0,"browsers":{"android":{"8.1":{}},"chrome":
+     {"100.0":{},"99.0":{}},"chrome-mobile":{"86.0":{}},"firefox":{"97.0":{},"98.0":{}},"opera":{"84.0":{},"85.0":{}}}}
+     3. check total is 20
+  */
+    @Test
+    void checkTotal() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithGiven() {
+        given().
+                when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotalWithSomeLogs() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkChromeVersion() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+
+    @Test
+    void checkTotalGoodPractice() {
+        Integer expectedTotal = 20;
+        Integer actualTotal = given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .path("total");
+    }
+       /*
+     1. make request to https://selenoid.autotests.cloud/wd/hub/status
+     2. get response {"value":{"message":"Selenoid 1.10.7 built at 2021-11-21_05:46:32AM","ready":true}}
+     3. check value.ready is true
+  */
+       @Test
+       void checkWDStatus() {
+           given()
+                   .log().uri()
+                   .when()
+                   .get("https://selenoid.autotests.cloud/wd/hub/status")
+                   .then()
+                   .log().status()
+                   .log().body()
+                   .statusCode(401);
+       }
+    @Test
+    void checkWDTotal() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://user1:1234@selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("value.ready", is(true));
+    }
+    @Test
+    void checkWDHubStatus() {
+        given()
+                .auth().basic("user1","1234")
+                .log().uri()
+                .when()
+                .get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("value.ready", is(true));
+    }
+}
